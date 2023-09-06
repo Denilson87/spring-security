4f4dc35cc7e0b1848ba351bb15304d5520644f71
Denilson <denilsonmucavel@outlook.com>
2023-09-06 21:05:20 +0200

    updating controller methods

--- src/main/java/com/heath/Patientsapi/controller/PatientController.java
+++ src/main/java/com/heath/Patientsapi/controller/PatientController.java
@@ -2,11 +2,9 @@ package com.heath.Patientsapi.controller;
 
 import com.heath.Patientsapi.dtos.PatientDto;
 import com.heath.Patientsapi.models.PatientModel;
-import com.heath.Patientsapi.repositories.PatientRepository;
 import com.heath.Patientsapi.service.PatientService;
 import jakarta.validation.Valid;
 import org.springframework.beans.BeanUtils;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
@@ -24,27 +22,56 @@ import java.util.UUID;
 
 public class PatientController {
 
-    @Autowired
-    PatientRepository patientRepository;
+final  PatientService patientService;
+
+    public PatientController(PatientService patientService) {
+        this.patientService = patientService;
+    }
 
     @PostMapping("/patient")
     public ResponseEntity<PatientModel> savePatient(@RequestBody @Valid PatientDto patientDto) {
         var patientModel = new PatientModel();
         BeanUtils.copyProperties(patientDto, patientModel);
-        return ResponseEntity.status(HttpStatus.CREATED).body(patientRepository.save(patientModel));
+        return ResponseEntity.status(HttpStatus.CREATED).body(patientService.save(patientModel));
 
         }
     @GetMapping("/patient")
     public ResponseEntity<Page<PatientModel>> getAllPatients(@PageableDefault(page = 0, size = 10, sort = "id", direction = Sort.Direction.ASC) Pageable pageable){
-        return ResponseEntity.status(HttpStatus.OK).body(patientRepository.findAll(pageable));
+        return ResponseEntity.status(HttpStatus.OK).body(patientService.findAll(pageable));
     }
 
     @GetMapping("/patient/{id}")
     public ResponseEntity<Object> getOnePatient(@PathVariable(value = "id") UUID id){
-        Optional<PatientModel> patientModelOptional = patientRepository.findById(id);
+        Optional<PatientModel> patientModelOptional = patientService.findById(id);
         if (!patientModelOptional.isPresent()) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Patient you're trying to get was not found.");
         }
         return ResponseEntity.status(HttpStatus.OK).body(patientModelOptional.get());
     }
+
+    @DeleteMapping("/patient/{id}")
+    public ResponseEntity<Object> deleteOnePatient(@PathVariable( value = "id") UUID id){
+        Optional<PatientModel> patientModelOptional = patientService.findById(id);
+        if (!patientModelOptional.isPresent()){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Patient you´re trying to delete does not exist!");
+        }
+        patientService.delete(patientModelOptional.get());
+        return ResponseEntity.status(HttpStatus.OK).body("Patient Deleted Succesfully!");
+    }
+
+    @PutMapping("patient/{id}")
+    public ResponseEntity<Object> updatePatient(@PathVariable(value = "id") UUID id,
+                                                 @RequestBody @Valid PatientDto patientDto){
+        Optional<PatientModel> patientModelOptional = patientService.findById(id);
+        if (!patientModelOptional.isPresent()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Patient not found.");
+        }
+        var patientModel = new PatientModel();
+        BeanUtils.copyProperties(patientDto, patientModel);
+        patientModel.setId(patientModelOptional.get().getId());
+        return ResponseEntity.status(HttpStatus.OK).body(patientService.save(patientModel));
+    }
+
+
+
 }
--- src/main/java/com/heath/Patientsapi/service/PatientService.java
+++ src/main/java/com/heath/Patientsapi/service/PatientService.java
@@ -2,174 +2,35 @@ package com.heath.Patientsapi.service;
 
 import com.heath.Patientsapi.models.PatientModel;
 import com.heath.Patientsapi.repositories.PatientRepository;
-import org.springframework.data.domain.Example;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
-import org.springframework.data.repository.query.FluentQuery;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
-import java.util.function.Function;
 
 @Service
 public class PatientService {
 
     final PatientRepository patientRepository;
-
-    public PatientService (PatientRepository patientRepository){
-        this.patientRepository = new PatientRepository() {
-            @Override
-            public void flush() {
-
-            }
-
-            @Override
-            public <S extends PatientModel> S saveAndFlush(S entity) {
-                return null;
-            }
-
-            @Override
-            public <S extends PatientModel> List<S> saveAllAndFlush(Iterable<S> entities) {
-                return null;
-            }
-
-            @Override
-            public void deleteAllInBatch(Iterable<PatientModel> entities) {
-
-            }
-
-            @Override
-            public void deleteAllByIdInBatch(Iterable<UUID> uuids) {
-
-            }
-
-            @Override
-            public void deleteAllInBatch() {
-
-            }
-
-            @Override
-            public PatientModel getOne(UUID uuid) {
-                return null;
-            }
-
-            @Override
-            public PatientModel getById(UUID uuid) {
-                return null;
-            }
-
-            @Override
-            public PatientModel getReferenceById(UUID uuid) {
-                return null;
-            }
-
-            @Override
-            public <S extends PatientModel> List<S> findAll(Example<S> example) {
-                return null;
-            }
-
-            @Override
-            public <S extends PatientModel> List<S> findAll(Example<S> example, Sort sort) {
-                return null;
-            }
-
-            @Override
-            public <S extends PatientModel> List<S> saveAll(Iterable<S> entities) {
-                return null;
-            }
-
-            @Override
-            public List<PatientModel> findAll() {
-                return null;
-            }
-
-            @Override
-            public List<PatientModel> findAllById(Iterable<UUID> uuids) {
-                return null;
-            }
-
-            @Override
-            public <S extends PatientModel> S save(S entity) {
-                return null;
-            }
-
-            @Override
-            public Optional<PatientModel> findById(UUID uuid) {
-                return Optional.empty();
-            }
-
-            @Override
-            public boolean existsById(UUID uuid) {
-                return false;
-            }
-
-            @Override
-            public long count() {
-                return 0;
-            }
-
-            @Override
-            public void deleteById(UUID uuid) {
-
-            }
-
-            @Override
-            public void delete(PatientModel entity) {
-
-            }
-
-            @Override
-            public void deleteAllById(Iterable<? extends UUID> uuids) {
-
-            }
-
-            @Override
-            public void deleteAll(Iterable<? extends PatientModel> entities) {
-
-            }
-
-            @Override
-            public void deleteAll() {
-
-            }
-
-            @Override
-            public List<PatientModel> findAll(Sort sort) {
-                return null;
-            }
-
-            @Override
-            public Page<PatientModel> findAll(Pageable pageable) {
-                return  patientRepository.findAll(pageable);
-            }
-
-            @Override
-            public <S extends PatientModel> Optional<S> findOne(Example<S> example) {
-                return Optional.empty();
-            }
-
-            @Override
-            public <S extends PatientModel> Page<S> findAll(Example<S> example, Pageable pageable) {
-                return null;
-            }
-
-            @Override
-            public <S extends PatientModel> long count(Example<S> example) {
-                return 0;
-            }
-
-            @Override
-           public <S extends PatientModel> boolean exists(Example<S> example) {
-                return false;
-            }
-
-            @Override
-            public <S extends PatientModel, R> R findBy(Example<S> example, Function<FluentQuery.FetchableFluentQuery<S>, R> queryFunction) {
-                return null;
-            }
-        };
+    @Autowired
+    public PatientService(PatientRepository patientRepository) {
+        this.patientRepository = patientRepository;
+    }
+    @Transactional
+    public PatientModel save(PatientModel patientModel) {
+        return patientRepository.save(patientModel);
+    }
+    public Page<PatientModel> findAll(Pageable pageable) {
+        return  patientRepository.findAll(pageable);
+    }
+    public Optional<PatientModel> findById(UUID id) {
+        return patientRepository.findById(id);
+    }
+    @Transactional
+    public void delete(PatientModel patientModel) {
+        patientRepository.delete(patientModel);
     }
 }
