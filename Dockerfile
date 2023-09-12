cf655b7035844582e66925f3525c9fa3c78acd41
Denilson <denilsonmucavel@outlook.com>
2023-09-12 18:44:43 +0200

    updating the docker file

--- Dockerfile
+++ Dockerfile
@@ -1,15 +1,9 @@
 FROM ubuntu:latest AS build
-
 RUN apt-get update
 RUN apt-get install openjdk-17-jdk -y
-COPY . .
-
-RUN ./gradlew bootJar --no-daemon
 
 FROM openjdk:17-jdk-slim
 
+ADD target/patients-api.jar patients-api.jar
+ENTRYPOINT ["java","-jar","/patients-api.jar"]
 EXPOSE 8080
-
-COPY --from=build /build/libs/demo-1.jar app.jar
-
-ENTRYPOINT ["java", "-jar", "app.jar"]
--- src/main/java/com/heath/Patientsapi/PatientsApiApplication.java
+++ src/main/java/com/heath/Patientsapi/PatientsApiApplication.java
@@ -2,12 +2,15 @@ package com.heath.Patientsapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 public class PatientsApiApplication {
-
-	public static void main(String[] args) {
+	@GetMapping("/Hello")
+	public static String main(String[] args) {
 		SpringApplication.run(PatientsApiApplication.class, args);
+
+		return "Hello world";
 	}
 
 }
